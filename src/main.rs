fdb4724ffcb489517100bfa6279e67e18237134b
manju malepati <manjumalepati17@gmail.com>
2020-04-10 13:43:04 +0300

    Implemented structure config and improvised error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use std::env;
 use std::io;
+use std::fs;
+use std::process;
+#[derive(Debug)]
+struct Config{
+    query:String,
+    filename:String
+}
+
+impl Config{
+    fn new(args:&Vec<String>)->Result<Config,&'static str>{
+        if args.len()<3{
+           return Err("Not enough arguments");
+        }
+        let _query = args[1].clone();
+        let file_name = args[2].clone();
+        let config = Config{
+            query:_query,
+            filename:file_name,
+        };
+        Ok(config)
+    }//end of new function
+}
 fn main() {
     println!("Hello, world!");
     let args:Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
-    println!("Query to search:{}",query);
-    println!("In File Name:{}",file_name);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments:{}",err);
+        process::exit(1);
+    });
+    let file_content = fs::read_to_string(config.filename).expect("Could not read from file");
+    println!("File content is:{}",file_content);
 }
