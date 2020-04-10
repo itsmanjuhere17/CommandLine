bf02594b3029e6d04e85e0b9932d7f517433ef9e
manju malepati <manjumalepati17@gmail.com>
2020-04-10 15:50:55 +0300

    Final Logic,Added test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use std::env;
 use std::fs;
-use std::process;
 //use std::error::Error;
 use std::io;
 
-#[derive(Debug)]
+#[derive(Debug,PartialEq)]
 pub struct Config{
     pub query:String,
     pub filename:String
@@ -25,8 +24,49 @@ impl Config{
     }//end of new function
 }
 
-pub fn run(config:&Config)->Result<(),io::Error> {
-    let file_content = fs::read_to_string(&config.filename)?;
-    println!("File content is:{}",file_content);
+pub fn run(config:Config)->Result<(),io::Error> {
+    let file_content = fs::read_to_string(config.filename)?;
+    for hit in search(&config.query,&file_content){
+        println!("{}",hit);
+    }
     Ok(())
+}
+
+//Here, if we don't specify lifetime, rust wont compile as it does not know till what lifetime the return value should be contained.
+pub fn search<'a>(query:&str,content:&'a str)->Vec<&'a str>{
+    let mut res = Vec::new();
+    for line in content.lines(){
+        if line.contains(query){
+            res.push(line);
+        }
+    }
+    res
+}
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn one_result(){
+        let query="duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."],search(query,content));
+    }
+    #[test]
+    fn test_config_new_error(){
+        let empty = Vec::new();
+        let res = Config::new(&empty);
+        assert_eq!(Err("Not enough arguments"),res);
+    }
+
+    #[test]
+    fn test_config_new_ok(){
+        let args = vec!["CommandLineProject".to_string(),"hello".to_string(),"hello_world.txt".to_string()];
+        assert_eq!(Ok(Config{
+            query:args[1].to_string(),
+            filename:args[2].to_string()
+        }), Config::new(&args));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use CommandLineProject::Config; //Library Crate.
 use std::env;
 use std::process;
 fn main() {
-    println!("Hello, world!");
     let args:Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err|{
         println!("Problem parsing arguments:{}",err);
         process::exit(1);
     });
-    if let Err(err)=CommandLineProject::run(&config){
+    if let Err(err)=CommandLineProject::run(config){
         println!("Error running application:{}",err);
         process::exit(1);
     }
 }
+
 
