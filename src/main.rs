3e01bebb8a84b3fba5ec933653f8e61b273768cd
manju malepati <manjumalepati17@gmail.com>
2020-04-10 11:55:23 +0300

    Accepting command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+use std::io;
 fn main() {
     println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_name = &args[2];
+    println!("Query to search:{}",query);
+    println!("In File Name:{}",file_name);
 }
